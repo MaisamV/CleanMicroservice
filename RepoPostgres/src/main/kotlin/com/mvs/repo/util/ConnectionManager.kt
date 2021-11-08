9081441f906f6245951a8b63489f546419e92820
MaisamV <maisam.vahidsafa@gmail.com>
2021-11-08 18:54:36 +0330

    Added ConnectionManager.kt utility which handles jdbc connections to database

---
+++ RepoPostgres/src/main/kotlin/com/mvs/repo/util/ConnectionManager.kt
@@ -0,0 +1,57 @@
+package com.mvs.repo.util
+
+import org.apache.commons.dbcp2.BasicDataSource
+import org.postgresql.Driver
+import java.sql.Connection
+import java.sql.DriverManager
+
+object ConnectionManager {
+    val dataSource = initDb()
+
+    fun <R> use(block: (Connection) -> R): R {
+        return use(false, block)
+    }
+
+    fun <R> useAutoCommit(block: (Connection) -> R): R {
+        return use(true, block)
+    }
+
+    private inline fun <R> use(autoCommit: Boolean, block: (Connection) -> R): R {
+        return dataSource.connection.use {
+            it.autoCommit = autoCommit
+            block.invoke(it)
+        }
+    }
+
+    private fun initDb(): BasicDataSource {
+        DriverManager.registerDriver(Driver())
+
+        val dataSource = BasicDataSource()
+        dataSource.maxTotal = 90
+        dataSource.maxIdle = 90
+        dataSource.url = "jdbc:postgresql://${System.getenv("fund_db_host")}:${System.getenv("fund_db_port")}/${System.getenv("fund_db_name")}" //example: "jdbc:postgresql://192.168.240.173:5432/postgres"
+        dataSource.username = System.getenv("fund_db_user_name")
+        dataSource.password = System.getenv("fund_db_user_pass")
+        return dataSource
+    }
+}
+
+inline fun <T : AutoCloseable?, R> T.use(block: (T) -> R): R {
+    var exception: Throwable? = null
+    try {
+        return block(this)
+    } catch (e: Throwable) {
+        exception = e
+        throw e
+    } finally {
+        when {
+            this == null -> {}
+            exception == null -> close()
+            else ->
+                try {
+                    close()
+                } catch (closeException: Throwable) {
+                }
+        }
+    }
+}
