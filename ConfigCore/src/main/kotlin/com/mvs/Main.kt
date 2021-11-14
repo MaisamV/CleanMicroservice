4cca0f35c514954b227db58c6a2518fccf039ee2
MaisamV <maisam.vahidsafa@gmail.com>
2021-11-14 14:49:55 +0330

    Made application to start both http and grpc server at the same time

--- ConfigCore/src/main/kotlin/com/mvs/Main.kt
+++ ConfigCore/src/main/kotlin/com/mvs/Main.kt
@@ -1,6 +1,14 @@
+import com.mvs.di.GrpcDeliveryProvider
 import com.mvs.di.KtorDeliveryProvider
+import kotlin.concurrent.thread
 
 fun main(args: Array<String>) {
-    val delivery = KtorDeliveryProvider().provide()
-    delivery.start(args)
+    thread {
+        val delivery = GrpcDeliveryProvider().provide()
+        delivery.start(args)
+    }
+    thread {
+        val delivery = KtorDeliveryProvider().provide()
+        delivery.start(args)
+    }
 }
