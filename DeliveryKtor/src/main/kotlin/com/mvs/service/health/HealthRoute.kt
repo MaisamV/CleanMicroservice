ba3c78db8a583ac3589990e38d5a4410ef669285
MaisamV <maisam.vahidsafa@gmail.com>
2021-11-20 18:18:19 +0330

    Made health and ping routes use Kompendium extensions

--- DeliveryKtor/src/main/kotlin/com/mvs/service/health/HealthRoute.kt
+++ DeliveryKtor/src/main/kotlin/com/mvs/service/health/HealthRoute.kt
@@ -2,34 +2,18 @@ package com.mvs.service.health
 
 import com.mvs.health.IHealthCommand
 import com.mvs.service.DocExample
-import com.mvs.service.dto.PingErrorSheet
 import com.mvs.service.util.addRoute
-import com.mvs.service.util.respondError
 import com.mvs.service.util.respondOk
-import io.bkbn.kompendium.Notarized.notarizedGet
+import com.mvs.service.util.xGet
 import io.ktor.application.*
 import io.ktor.routing.*
-import io.ktor.util.pipeline.*
 
 fun Route.healthRoutes(command: IHealthCommand) {
     route("/api") {
         addRoute("/health") {
-            notarizedGet(DocExample.getExamples) {
-                handleHealth(command)
+            xGet(DocExample.getExamples) {
+                call.respondOk(command.checkHealth())
             }
         }
-    }
-}
-
-suspend fun PipelineContext<Unit, ApplicationCall>.handleHealth(command: IHealthCommand) {
-    try {
-        call.respondOk(
-            command.checkHealth()
-        )
-    } catch (e: Exception) {
-        e.printStackTrace()
-        call.respondError(
-            PingErrorSheet.PING_EXCEPTION.toErrorData()
-        )
     }
 }
--- DeliveryKtor/src/main/kotlin/com/mvs/service/health/PingRoute.kt
+++ DeliveryKtor/src/main/kotlin/com/mvs/service/health/PingRoute.kt
@@ -2,34 +2,18 @@ package com.mvs.service.health
 
 import com.mvs.health.IPingCommand
 import com.mvs.service.DocExample
-import com.mvs.service.dto.HealthErrorSheet
 import com.mvs.service.util.addRoute
-import com.mvs.service.util.respondError
 import com.mvs.service.util.respondOk
-import io.bkbn.kompendium.Notarized.notarizedGet
+import com.mvs.service.util.xGet
 import io.ktor.application.*
 import io.ktor.routing.*
-import io.ktor.util.pipeline.*
 
 fun Route.pingRoute(command: IPingCommand) {
     route("/api") {
         addRoute("/ping") {
-            notarizedGet(DocExample.getExamples) {
-                handlePing(command)
+            xGet(DocExample.getExamples) {
+                call.respondOk(command.ping())
             }
         }
-    }
-}
-
-suspend fun PipelineContext<Unit, ApplicationCall>.handlePing(command: IPingCommand) {
-    try {
-        call.respondOk(
-            command.ping()
-        )
-    } catch (e: Exception) {
-        e.printStackTrace()
-        call.respondError(
-            HealthErrorSheet.HEALTH_EXCEPTION.toErrorData()
-        )
     }
 }
