90625256fa4e05d16471dc9069fb6d022d96d388
MaisamV <maisam.vahidsafa@gmail.com>
2021-11-20 18:12:53 +0330

    Added notarizedPatch extension

--- DeliveryKtor/src/main/kotlin/com/mvs/service/util/RouteExtension.kt
+++ DeliveryKtor/src/main/kotlin/com/mvs/service/util/RouteExtension.kt
@@ -1,6 +1,14 @@
 package com.mvs.service.util
 
+import io.bkbn.kompendium.Kompendium
+import io.bkbn.kompendium.KompendiumPreFlight
+import io.bkbn.kompendium.MethodParser
+import io.bkbn.kompendium.models.meta.MethodInfo
+import io.bkbn.kompendium.models.oas.OpenApiSpecPathItem
+import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.routing.*
+import io.ktor.util.pipeline.*
 
 fun Route.addRoute(path: String, build: Route.() -> Unit) {
     if(path.endsWith('/')) {
@@ -8,4 +16,17 @@ fun Route.addRoute(path: String, build: Route.() -> Unit) {
     }
     route(path, build)
     route("$path/", build)
+}
+
+inline fun <reified TParam : Any, reified TReq : Any, reified TResp : Any> Route.notarizedPatch(
+    info: MethodInfo.PostInfo<TParam, TReq, TResp>,
+    noinline body: PipelineInterceptor<Unit, ApplicationCall>
+): Route {
+    return KompendiumPreFlight.methodNotarizationPreFlight<TParam, TReq, TResp>() { paramType, requestType, responseType ->
+        val path = Kompendium.calculatePath(this)
+        Kompendium.openApiSpec.paths.getOrPut(path) { OpenApiSpecPathItem() }
+        Kompendium.openApiSpec.paths[path]?.patch =
+            MethodParser.parseMethodInfo(info, paramType, requestType, responseType)
+        return method(HttpMethod.Patch) { handle(body) }
+    }
 }
