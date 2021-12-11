5fcf182ed87e06258d768f65085baa658efd595d
MaisamV <maisam.vahidsafa@gmail.com>
2021-12-11 17:12:09 +0330

    Added AnyExtension functions

---
+++ Model/src/main/kotlin/com/mvs/model/AnyExtension.kt
@@ -0,0 +1,26 @@
+package com.mvs.model
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
+
+@OptIn(ExperimentalContracts::class)
+inline fun <T: Any?> T.isNotNull(): Boolean {
+    contract {
+        returns(true) implies (this@isNotNull != null)
+    }
+    return this != null
+}
+
+@OptIn(ExperimentalContracts::class)
+inline fun <T: Any?> T.isNull(): Boolean {
+    contract {
+        returns(true) implies (this@isNull == null)
+    }
+    return this == null
+}
+
+inline fun printStacktrace(msg: String?) {
+    println(msg)
+    Thread.dumpStack()
+}
