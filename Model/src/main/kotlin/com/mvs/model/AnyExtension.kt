1a7e205a3653cfd9f21551cc1e0cc0a37bf1b1de
MaisamV <maisam.vahidsafa@gmail.com>
2021-12-13 17:10:51 +0330

    Fixed isNull contract

--- Model/src/main/kotlin/com/mvs/model/AnyExtension.kt
+++ Model/src/main/kotlin/com/mvs/model/AnyExtension.kt
@@ -15,7 +15,7 @@ inline fun <T: Any?> T.isNotNull(): Boolean {
 @OptIn(ExperimentalContracts::class)
 inline fun <T: Any?> T.isNull(): Boolean {
     contract {
-        returns(true) implies (this@isNull == null)
+        returns(false) implies (this@isNull != null)
     }
     return this == null
 }
