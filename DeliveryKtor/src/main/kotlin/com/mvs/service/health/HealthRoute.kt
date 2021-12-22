1dd5663d3aea595a081820b3424014c86dc153b3
MaisamV <maisam.vahidsafa@gmail.com>
2021-12-22 17:03:31 +0330

    Used serviceRoute in health and ping routes

--- DeliveryKtor/src/main/kotlin/com/mvs/service/health/HealthRoute.kt
+++ DeliveryKtor/src/main/kotlin/com/mvs/service/health/HealthRoute.kt
@@ -2,33 +2,30 @@ package com.mvs.service.health
 
 import com.mvs.auth.UserClaim
 import com.mvs.health.IHealthCommand
-import com.mvs.health.IPingCommand
 import com.mvs.model.health.HealthInfo
-import com.mvs.service.commandFactory
 import com.mvs.service.util.*
 import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
-import ir.sabaolgoo.ICommandFactory
 import java.util.*
 
 fun NormalOpenAPIRoute.healthRoutes() {
-    addRoute("/api/health") {
-        val code = 0x10001L
-        xPost<Unit, HealthInfo, Unit> { _, _ ->
-            val commandFactory: ICommandFactory<IHealthCommand> = commandFactory.getCommandFactory(IHealthCommand::class) as ICommandFactory<IHealthCommand>
-            val command = commandFactory.create(UserClaim())
-            respondOk(command.checkHealth())
-        }.jsonParams<ServiceInfoModel> {
-            created = GregorianCalendar(2021, 11, 23).time
-            serviceCode = code
-        }
+    serviceRoute<IHealthCommand> { factory ->
+        addRoute("/api/health") {
+            val code = 0x10001L
+            xPost<Unit, HealthInfo, Unit> { _, _ ->
+                val command = factory.create(UserClaim())
+                respondOk(command.checkHealth())
+            }.jsonParams<ServiceInfoModel> {
+                created = GregorianCalendar(2021, 11, 23).time
+                serviceCode = code
+            }
 
-        xGet<Unit, HealthInfo> {
-            val commandFactory: ICommandFactory<IHealthCommand> = commandFactory.getCommandFactory(IHealthCommand::class) as ICommandFactory<IHealthCommand>
-            val command = commandFactory.create(UserClaim())
-            respondOk(command.checkHealth())
-        }.jsonParams<ServiceInfoModel> {
-            created = GregorianCalendar(2021, 11, 23).time
-            serviceCode = code
+            xGet<Unit, HealthInfo> {
+                val command = factory.create(UserClaim())
+                respondOk(command.checkHealth())
+            }.jsonParams<ServiceInfoModel> {
+                created = GregorianCalendar(2021, 11, 23).time
+                serviceCode = code
+            }
         }
     }
 }
--- DeliveryKtor/src/main/kotlin/com/mvs/service/health/PingRoute.kt
+++ DeliveryKtor/src/main/kotlin/com/mvs/service/health/PingRoute.kt
@@ -6,19 +6,21 @@ import com.mvs.health.IPingCommand
 import com.mvs.service.commandFactory
 import com.mvs.service.util.*
 import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
+import ir.sabaolgoo.ICommand
 import ir.sabaolgoo.ICommandFactory
 import java.util.*
 
 fun NormalOpenAPIRoute.pingRoute() {
-    addRoute("/api/ping") {
-        val code = 0x10000L
-        xGet<Unit, Boolean> {
-            val commandFactory: ICommandFactory<IPingCommand> = commandFactory.getCommandFactory(IPingCommand::class) as ICommandFactory<IPingCommand>
-            val command = commandFactory.create(UserClaim())
-            respondOk(command.ping())
-        }.jsonParams<ServiceInfoModel> {
-            created = GregorianCalendar(2021, 11, 23).time
-            serviceCode = code
+    serviceRoute<IPingCommand> { factory ->
+        addRoute("/api/ping") {
+            val code = 0x10000L
+            xGet<Unit, Boolean> {
+                val command = factory.create(UserClaim())
+                respondOk(command.ping())
+            }.jsonParams<ServiceInfoModel> {
+                created = GregorianCalendar(2021, 11, 23).time
+                serviceCode = code
+            }
         }
     }
 }
