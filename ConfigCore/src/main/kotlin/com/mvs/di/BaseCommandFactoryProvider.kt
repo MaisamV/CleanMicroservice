2fcaeb452853e49ae7de0218c9b2934ec7035ddc
MaisamV <maisam.vahidsafa@gmail.com>
2021-12-22 14:58:15 +0330

    Added BaseCommandFactoryProvider.kt

---
+++ ConfigCore/src/main/kotlin/com/mvs/di/BaseCommandFactoryProvider.kt
@@ -0,0 +1,18 @@
+package com.mvs.di
+
+import com.mvs.health.IHealthCommand
+import com.mvs.health.IPingCommand
+import ir.sabaolgoo.ICommand
+import ir.sabaolgoo.ICommandFactory
+import ir.sabaolgoo.ICommandFactoryProvider
+import kotlin.reflect.KClass
+
+class BaseCommandFactoryProvider: ICommandFactoryProvider {
+    override fun <TCommand : ICommand> getCommandFactory(c: KClass<TCommand>): ICommandFactory<*> {
+        return when (c) {
+            IPingCommand::class -> PingCommandFactory()
+            IHealthCommand::class -> HealthCommandFactory()
+            else -> throw IllegalArgumentException()
+        }
+    }
+}
