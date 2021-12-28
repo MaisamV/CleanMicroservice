a81db2f0d638a1137613ea40e8e2e032bc5b35da
MaisamV <maisam.vahidsafa@gmail.com>
2021-12-28 12:45:22 +0330

    Used grpc base dto in health and ping services

--- DeliveryGrpc/src/main/kotlin/com/mvs/delivery/service/HealthService.kt
+++ DeliveryGrpc/src/main/kotlin/com/mvs/delivery/service/HealthService.kt
@@ -1,16 +1,21 @@
 package com.mvs.delivery.service
 
-import com.mvs.delivery.HealthGrpc
-import com.mvs.delivery.HealthRequest
-import com.mvs.delivery.HealthResponse
-import com.mvs.delivery.toDto
+import com.mvs.delivery.*
 import com.mvs.health.IHealthCommand
 import io.grpc.stub.StreamObserver
 
 class HealthService(private val command: IHealthCommand) : HealthGrpc.HealthImplBase() {
-    override fun health(req: HealthRequest, responseObserver: StreamObserver<HealthResponse?>) {
-        val healthInfo = command.checkHealth()
-        responseObserver.onNext(healthInfo.toDto())
-        responseObserver.onCompleted()
+    override fun health(req: HealthRequest, responseObserver: StreamObserver<HealthDto>?) {
+        respond(responseObserver, {
+            HealthDto.newBuilder()
+                .setIsSuccessful(false)
+                .setError(it)
+                .build()
+        }) {
+            HealthDto.newBuilder()
+                .setIsSuccessful(true)
+                .setData(command.checkHealth().toDto())
+                .build()
+        }
     }
 }
--- DeliveryGrpc/src/main/kotlin/com/mvs/delivery/service/PingService.kt
+++ DeliveryGrpc/src/main/kotlin/com/mvs/delivery/service/PingService.kt
@@ -1,15 +1,24 @@
 package com.mvs.delivery.service
 
+import com.mvs.delivery.PingDto
 import com.mvs.delivery.PingGrpc
 import com.mvs.delivery.PingRequest
-import com.mvs.delivery.PingResponse
+import com.mvs.delivery.respond
 import com.mvs.health.IPingCommand
 import io.grpc.stub.StreamObserver
 
 class PingService(val command: IPingCommand) : PingGrpc.PingImplBase() {
-    override fun ping(req: PingRequest, responseObserver: StreamObserver<PingResponse?>) {
-        val reply = PingResponse.newBuilder().setIsSuccessful(command.ping()).build()
-        responseObserver.onNext(reply)
-        responseObserver.onCompleted()
+    override fun ping(req: PingRequest, responseObserver: StreamObserver<PingDto>?) {
+        respond(responseObserver, {
+            PingDto.newBuilder()
+                .setIsSuccessful(false)
+                .setError(it)
+                .build()
+        }) {
+            PingDto.newBuilder()
+                .setIsSuccessful(true)
+                .setData(command.ping())
+                .build()
+        }
     }
 }
