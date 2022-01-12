c56bfc4af0f35e347cbb2c17e36be8c98afe7bcb
MaisamV <maisam.vahidsafa@gmail.com>
2022-01-12 18:00:58 +0330

    Changed gitlab address

--- CloneTemplate.bat
+++ CloneTemplate.bat
@@ -3,12 +3,12 @@ cd ./CleanMicroservice
 git remote rename origin github
 git branch -m base
 git remote set-url --add --push github https://github.com/MaisamV/CleanMicroservice.git
-git remote set-url --add --push github http://git.sabaolgoo.ir/m.vahidsafa/microservicetemplate.git
+git remote set-url --add --push github http://scg.otcsaba.ir/m.vahidsafa/microservicetemplate.git
 git branch master
 git checkout master
 @echo off
 set /p repository="Enter Project name in gitlab: "
-git remote add origin http://git.sabaolgoo.ir/m.vahidsafa/%repository%
+git remote add origin http://scg.otcsaba.ir/m.vahidsafa/%repository%
 git push -u origin master
 cd ..
 ren CleanMicroservice %repository%
