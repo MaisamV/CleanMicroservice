660a3451f755e3f9357fce23b72c8aec9b4b57c5
MaisamV <maisam.vahidsafa@gmail.com>
2022-01-22 13:57:00 +0330

    Changed RoleType enum codes

--- Model/src/main/kotlin/com/mvs/auth/RoleType.kt
+++ Model/src/main/kotlin/com/mvs/auth/RoleType.kt
@@ -1,10 +1,10 @@
 package com.mvs.auth
 
 enum class RoleType(val code: Long) {
-    SUPER_ADMIN(0),
-    ADMIN(1),
-    NORMAL(20000),
-    ANONYMOUS(Long.MAX_VALUE);
+    SUPER_ADMIN(Long.MAX_VALUE),
+    ADMIN(20000),
+    NORMAL(1),
+    ANONYMOUS(0);
 
     companion object {
         fun from(code: Long): RoleType = RoleType.values().first { it.code == code }
