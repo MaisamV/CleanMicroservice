2991a78483a59eee0a5781ff7a6bb679f1caabea
MaisamV <maisam.vahidsafa@gmail.com>
2022-01-22 16:55:02 +0330

    Added database migrate before starting the servers

--- ConfigCore/src/main/kotlin/com/mvs/Main.kt
+++ ConfigCore/src/main/kotlin/com/mvs/Main.kt
@@ -2,9 +2,16 @@ package com.mvs
 
 import com.mvs.di.GrpcDeliveryProvider
 import com.mvs.di.KtorDeliveryProvider
+import com.mvs.model.env
+import migrateDb
 import kotlin.concurrent.thread
 
 fun main(args: Array<String>) {
+    migrateDb(
+        env("db_url")!!,
+        env("db_superuser_name")!!,
+        env("db_superuser_name")!!
+    )
     thread {
         val delivery = GrpcDeliveryProvider().provide()
         delivery.start(args)
