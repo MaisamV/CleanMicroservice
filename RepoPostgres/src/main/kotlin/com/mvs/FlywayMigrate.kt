328944a48cb2f829f9f0baa7ff12943c5a1d89bc
MaisamV <maisam.vahidsafa@gmail.com>
2022-01-23 11:57:54 +0330

    Moved flyway migration scripts to resource directory in order to programmatically access them

--- RepoPostgres/src/main/kotlin/com/mvs/FlywayMigrate.kt
+++ RepoPostgres/src/main/kotlin/com/mvs/FlywayMigrate.kt
@@ -2,7 +2,7 @@ import org.flywaydb.core.Flyway
 
 
 fun main() {
-    val jdbcUrl = "jdbc:postgresql://${System.getenv("db_host")}:${System.getenv("db_port")}/${System.getenv("db_name")}"
+    val jdbcUrl = System.getenv("db_url")
     val username = System.getenv("db_superuser_name")
     val pass = System.getenv("db_superuser_pass")
     migrateDb(jdbcUrl, username, pass)
