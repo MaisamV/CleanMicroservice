7e6ea7cfab2459ed0341a8d11622b43449ba5b71
MaisamV <maisam.vahidsafa@gmail.com>
2022-01-24 19:16:27 +0330

    Used new extension functions in Health and ping routes

--- DeliveryKtor/src/main/kotlin/com/mvs/service/health/HealthRoute.kt
+++ DeliveryKtor/src/main/kotlin/com/mvs/service/health/HealthRoute.kt
@@ -1,17 +1,20 @@
 package com.mvs.service.health
 
-import com.mvs.auth.UserClaim
 import com.mvs.health.IHealthCommand
 import com.mvs.service.commandFactory
 import com.mvs.service.util.addRoute
+import com.mvs.service.util.authGet
+import com.mvs.service.util.ktorService
 import com.mvs.service.util.respondOk
 import io.ktor.application.*
 import io.ktor.routing.*
 
 fun Route.healthRoutes() {
-    addRoute("/api/health") {
-        get {
-            call.respondOk(commandFactory.getCommandFactory(IHealthCommand::class).create(UserClaim()).checkHealth())
+    ktorService(commandFactory.getCommandFactory(IHealthCommand::class)) { factory ->
+        addRoute("/api/health") {
+            authGet(factory) { command , claim ->
+                call.respondOk(command.checkHealth())
+            }
         }
     }
 }
--- DeliveryKtor/src/main/kotlin/com/mvs/service/health/PingRoute.kt
+++ DeliveryKtor/src/main/kotlin/com/mvs/service/health/PingRoute.kt
@@ -1,17 +1,20 @@
 package com.mvs.service.health
 
-import com.mvs.auth.UserClaim
 import com.mvs.health.IPingCommand
 import com.mvs.service.commandFactory
 import com.mvs.service.util.addRoute
+import com.mvs.service.util.authGet
+import com.mvs.service.util.ktorService
 import com.mvs.service.util.respondOk
 import io.ktor.application.*
 import io.ktor.routing.*
 
 fun Route.pingRoute() {
-    addRoute("/api/ping") {
-        get {
-            call.respondOk(commandFactory.getCommandFactory(IPingCommand::class).create(UserClaim()).ping())
+    ktorService(commandFactory.getCommandFactory(IPingCommand::class)) { factory ->
+        addRoute("/api/ping") {
+            authGet(factory) { command , claim ->
+                call.respondOk(command.ping())
+            }
         }
     }
 }
