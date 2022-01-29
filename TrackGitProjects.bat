aeb363175ee88411de652b7110b391090fd1c101
MaisamV <maisam.vahidsafa@gmail.com>
2022-01-29 11:28:10 +0330

    Changed TrackGitProjects script to track instead of push

--- TrackGitProjects.bat
+++ TrackGitProjects.bat
@@ -6,12 +6,11 @@ git remote add saba-template http://scg.otcsaba.ir/m.vahidsafa/microservicetempl
 git fetch saba-template
 git switch -c saba-base saba-template/master
 
-git branch master
-git checkout master
 @echo off
 set /p repository="Enter Project name in gitlab: "
 git remote add origin http://scg.otcsaba.ir/m.vahidsafa/%repository%
-git checkout --track origin/master
-git push -u origin master
+git fetch origin
+git switch -c master origin/master
+
 cd ..
 ren "%repoDir%" "%repository%"
