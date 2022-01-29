63a84d897996b2bfadc4406ae11d861ad175f0a1
MaisamV <maisam.vahidsafa@gmail.com>
2022-01-29 14:12:56 +0330

    Fixed track scripts

--- TrackGitProjects.bat
+++ TrackGitProjects.bat
@@ -3,23 +3,22 @@ git remote rename origin tempRemote
 git branch -m temp
 
 git remote add github https://github.com/MaisamV/CleanMicroservice.git
-git fetch github
-git switch -c base github/master
+git fetch github master:base
+git branch --set-upstream-to=github/master base
 
 git checkout base
 git branch -d temp
 git remote remove tempRemote
 
 git remote add saba-template http://scg.otcsaba.ir/m.vahidsafa/microservicetemplate.git
-git fetch saba-template
-git switch -c saba-base saba-template/master
+git fetch saba-template master:saba-base
+git branch --set-upstream-to=saba-template/master saba-base
 
 @echo off
 set /p repository="Enter Project name in gitlab: "
+git remote add origin http://scg.otcsaba.ir/m.vahidsafa/%repository%
+git fetch origin master:master
+git branch --set-upstream-to=origin/master master
+git checkout master
 cd ..
 ren "%repoDir%" "%repository%"
-cd "%repository%"
-
-git remote add origin http://scg.otcsaba.ir/m.vahidsafa/%repository%
-git fetch origin
-git switch -c master origin/master
