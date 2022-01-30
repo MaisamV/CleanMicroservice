95c3bef637c05c9540416679900d809215f28547
MaisamV <maisam.vahidsafa@gmail.com>
2022-01-30 14:04:28 +0330

    Replaced ServiceInfoModel Dates and code with String

--- DeliveryKtor/src/main/kotlin/com/mvs/service/util/ServiceInfoModel.kt
+++ DeliveryKtor/src/main/kotlin/com/mvs/service/util/ServiceInfoModel.kt
@@ -1,14 +1,18 @@
 package com.mvs.service.util
 
+import com.mvs.service.projectName
 import com.papsign.ktor.openapigen.model.operation.OperationModel
-import java.util.*
 
 class ServiceInfoModel(
-    var serviceCode: Long? = null,
-    var created: Date? = null,
-    var expired: Date? = null,
-    var modified: Date? = null,
+    var serviceCode: String? = null,
+    var created: String? = null,
+    var expired: String? = null,
+    var modified: String? = null,
     var forSandbox: Boolean = true,
     var supplier: Long = 0,
     var beta: Boolean = false,
-): OperationModel()
+): OperationModel() {
+    fun setServiceCode(value: Long) {
+        serviceCode = "${projectName}_$value"
+    }
+}
